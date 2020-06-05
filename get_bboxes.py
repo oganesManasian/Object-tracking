dcc06c07110907544a8827e97e4be906e992aecf
Oganes Manasian <oganesman8@gmail.com>
2020-06-05 23:53:59 +0200

    Refactored code

--- get_bboxes.py
+++ get_bboxes.py
@@ -13,12 +13,15 @@ import torchvision.transforms as transforms
 from YOLOv3.models import *
 from YOLOv3.utils.utils import rescale_boxes
 
+
 def torch_format(img):
     return transforms.ToTensor()(img)
+
 
 def resize(image, size=416):
     image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
     return image
+
 
 def pad_to_square(img, pad_value):
     c, h, w = img.shape
@@ -32,12 +35,13 @@ def pad_to_square(img, pad_value):
 
     return img, pad
 
+
 def get_bboxes(original_frame, bboxes):
     # Draw bounding boxes and labels of detections
     if bboxes is not None:
         # Rescale boxes to original image
-        detections = rescale_boxes(bboxes, 416, original_frame.shape[:2])        
-    
+        detections = rescale_boxes(bboxes, 416, original_frame.shape[:2])
+
     return detections
 
 
@@ -46,10 +50,10 @@ def parse_args():
     parser.add_argument("--video", type=str)
     parser.add_argument("--weights", type=str)
     parser.add_argument("--config", type=str)
-    
+
     args = parser.parse_args()
     return args
-    
+
 
 def main(args):
     # creating model on device
@@ -59,63 +63,62 @@ def main(args):
     model.load_darknet_weights(args.weights)
     model.eval()
     model.to(device)
-    
+
     # loading frames from video
     video_name = args.video
     print("Loading video ...")
     cap = cv2.VideoCapture(video_name)
-    
+
     # infer bboxes on video frames
     frame_number = 0
     while cap.isOpened():
         if frame_number % 100 == 0:
             print(f'Processing frame number {frame_number}')
-        
+
         ret, original_frame = cap.read()
-        
+
         if not ret:
             break
-        
+
         original_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2RGB)
-        
+
         frame = torch_format(original_frame)
         frame, pad = pad_to_square(frame, 0)
         frame = resize(frame).to(device)
-        
+
         detections = model(frame.unsqueeze(0))
         detections = non_max_suppression(detections, nms_thres=0.3, conf_thres=0.5)
         detections = detections[0]
-    
-        filename = f"outputs/{video_name.split('/')[-1]}_frame_{frame_number}"
-        
+
+        filename = f"bboxes/{video_name.split('/')[-1]}_frame_{frame_number}"
+
         if detections is None:
             np.save(filename, np.array([], dtype='float32'))
             break
-        
+
         # Taking only needed predictions
-        detections = detections[detections[:, -1] == 0] # Pedestrian class
+        detections = detections[detections[:, -1] == 0]  # Pedestrian class
         detections = detections[detections[:, -2] > 0.7]
         detections = detections[detections[:, -3] > 0.7]
-        
+
         # print('save...')
         get_bboxes(original_frame, detections).cpu().numpy()
         # np.save(filename, get_bboxes(original_frame, detections).cpu().numpy())
-        
+
         # Davit's code
-        bboxes = [{'x1':x1.item(),'y1':y1.item(),'x2':x2.item(),'y2':y2.item(), 'confidence': cls_conf.item()}
-                        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections]
-                        
+        bboxes = [{'x1': x1.item(), 'y1': y1.item(), 'x2': x2.item(), 'y2': y2.item(), 'confidence': cls_conf.item()}
+                  for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections]
+
         # Filter not valid boxes
         bboxes = [box for box in bboxes if abs(box['x2'] - box['x1']) > 0 and abs(box['y2'] - box['y1']) > 0]
         bboxes = [{key: max(float(value), 0) for key, value in box.items()} for box in bboxes]
-                  
+
         with open(f"{filename}.json", 'w') as f:
             json.dump({'children': bboxes}, f)
-            
-            
+
         frame_number += 1
-        
+
+
 if __name__ == "__main__":
     args = parse_args()
     main(args)
-    
--- get_ids.py
+++ get_ids.py
@@ -25,12 +25,15 @@ def extract_features(model, inputs):
     fm = model.global_avgpool(fm)
     return fm.view(fm.size(0), -1)
 
+
 def torch_format(img):
     return transforms.ToTensor()(img)
+
 
 def resize(image, size=416):
     image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
     return image
+
 
 def get_patch(image, bbox):
     # bbox = bbox.astype('int32')
@@ -38,27 +41,30 @@ def get_patch(image, bbox):
 
     return image[int(bbox['y1']):int(bbox['y2']), int(bbox['x1']):int(bbox['x2']), :]
 
+
 def normalize(image):
-    norm_mean = [0.485, 0.456, 0.406] # imagenet mean
-    norm_std = [0.229, 0.224, 0.225] # imagenet std
+    norm_mean = [0.485, 0.456, 0.406]  # imagenet mean
+    norm_std = [0.229, 0.224, 0.225]  # imagenet std
     return transforms.Normalize(mean=norm_mean, std=norm_std)(image)
 
+
 def patch_to_features(image, bbox, model):
-    
     # prepare path for network
     patch = get_patch(image, bbox)
     patch = torch_format(patch)
     patch = resize(patch, [256, 128])
-    
+
     # forward pass to get features
     patch = normalize(patch).cuda()
     patch_features = extract_features(model, patch.unsqueeze(0))
     patch_features = F.normalize(patch_features, p=2, dim=1)
-    
+
     return patch_features
+
 
 def build_features(image, bboxes, model):
     return torch.cat([patch_to_features(image, bbox, model) for bbox in bboxes], dim=0)
+
 
 def euclidean_squared_distance(input1, input2):
     """Computes euclidean squared distance.
@@ -75,11 +81,11 @@ def euclidean_squared_distance(input1, input2):
     distmat.addmm_(1, -2, input1, input2.t())
     return distmat
 
+
 def update_gallery(gallery, features, threshold=0.006):
-    
     dist = euclidean_squared_distance(gallery, features)
     new_features = features[torch.all(dist > threshold, dim=0)]
-    
+
     return torch.cat([gallery, new_features], dim=0)
 
 
@@ -88,23 +94,23 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--video", type=str)
     parser.add_argument("--weights", type=str)
-    
+
     args = parser.parse_args()
     return args
-    
-    
+
+
 def main(args):
     # Load model
     model = torchreid.models.build_model(
-                        name='resnet50',
-                        num_classes=1041,
-                        loss='softmax',
-                        pretrained=False)
-    
+        name='resnet50',
+        num_classes=1041,
+        loss='softmax',
+        pretrained=False)
+
     checkpoint = torch.load(args.weights)
     model.load_state_dict(checkpoint['state_dict'])
     model = model.cuda()
-    
+
     # Read video
     video_name = args.video
     frames, fps = read_video(video_name)
@@ -117,10 +123,10 @@ def main(args):
             data = json.load(f)
             bboxes = data['children']
             object_boxes_per_frame.append(bboxes)
-            
+
     # Run object tracking
     centroid_ids_per_frame = []
-        
+
     for frame_number, frame in enumerate(frames):
         if frame_number % 100 == 0:
             print(f'Processing frame number {frame_number}')
@@ -128,24 +134,27 @@ def main(args):
         image = frames[frame_number]
         bboxes = object_boxes_per_frame[frame_number]
         features = build_features(image, bboxes, model)
-        
-        if frame_number == 0: # Form gallery at the first frame
+
+        if frame_number == 0:  # Form gallery at the first frame
             gallery = features
             indexes = range(len(gallery))
         else:
-            if frame_number % 30 == 0: # Update gallery every 30 frames
+            if frame_number % 30 == 0:  # Update gallery every 30 frames
                 gallery = update_gallery(gallery, features)
-                
+
             dist_matrix = euclidean_squared_distance(gallery, features)
             indexes = torch.argmin(dist_matrix, dim=0).cpu().numpy()
-        
-        centroid_ids_per_frame.append([[((box['x1'] + box['x2']) / 2, (box['y1'] + box['y2']) / 2), index] for box, index in zip(bboxes, indexes)])
-        
+
+        centroid_ids_per_frame.append(
+            [[((box['x1'] + box['x2']) / 2, (box['y1'] + box['y2']) / 2), index] for box, index in
+             zip(bboxes, indexes)])
+
     print(f"Processed {len(centroid_ids_per_frame)} frames")
-    
+
     annotated_frames = annotate_frames(frames, object_boxes_per_frame, centroid_ids_per_frame)
     frames2video(annotated_frames, fps=28, filepath="result.avi")
     print("Created output video")
+
 
 if __name__ == "__main__":
     args = parse_args()
